43d6d7082df312747bd87ad2ed72f8e56319f3cd
SalvaCarsi <salvavcarsi@gmail.com>
2017-02-18 18:18:45 +0100

    the square shape component

---
+++ src/components/Circle.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import styled from 'styled-components';
+
+const Circle = styled.div`
+  margin: 0.15em;
+  border: 1px solid #000;
+  border-radius: 50%;
+	width: 2.5em;
+	height: 2.5em; 
+	background-color: #7E3517;
+`;
+
+export default Circle;
---
+++ src/components/TableWrapper.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import styled from 'styled-components';
+
+const TableWrapper = styled.table`
+    margin-top: 1.5em;
+    margin-left:auto; 
+    margin-right:auto;
+`;
+
+export default TableWrapper;
