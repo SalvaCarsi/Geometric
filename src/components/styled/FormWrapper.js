d5ab0f1e8fd7920d6a92245b049572fdb9374669
SalvaCarsi <salvavcarsi@gmail.com>
2017-02-19 20:58:41 +0100

    responsive form

--- src/components/styled/FormWrapper.js
+++ src/components/styled/FormWrapper.js
@@ -8,7 +8,14 @@ const FormWrapper = styled.div`
   padding: 0.1em 0.1em;
   margin: auto 2em auto 2em;
   background: #E8E8E8;
-  height: 530px;
+  
+  @media (max-width: 768px) {
+    height: 400px;
+  }
+  @media (min-width: 769px) {
+    height: 530px;
+  }
+  
 `;
 
 export default FormWrapper;
