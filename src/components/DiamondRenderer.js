2b7ed0965a83658ee68d1cf2211bfd382697e924
SalvaCarsi <salvavcarsi@gmail.com>
2017-02-19 10:04:43 +0100

    lodash, helpers file and setting the diamond renderer

--- src/components/DiamondRenderer.js
+++ src/components/DiamondRenderer.js
@@ -2,11 +2,41 @@
 
 import React, { Component } from 'react';
 
+import Circle from './styled/Circle';
+import ButtonsWrapper from './styled/ButtonsWrapper';
+import TableWrapper from './styled/TableWrapper';
+
+import generateArray from '../helpers';
+
 export class DiamondRenderer extends Component {
+
+  renderDiamond = () => {
+    return (
+      <TableWrapper>
+        <tbody>
+        {
+          generateArray().map( (value, index) => {
+            return (
+              <tr key={index}>
+                <td key={index}>
+                  {
+                    generateArray().map( (v, i) => {
+                      return <Circle/>;
+                    })
+                  }
+                </td>
+              </tr>
+            )
+          })
+        }
+        </tbody>
+      </TableWrapper>
+    );
+  };
 
   render = () => {
     return (
-      <h1>This is going to be a 5x5 diamond!</h1>
+      <ButtonsWrapper>{this.renderDiamond()}</ButtonsWrapper>
     );
   }
 
